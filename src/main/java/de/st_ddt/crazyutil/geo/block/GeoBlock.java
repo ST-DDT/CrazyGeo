acd5d05f7b31d7eff93f036f6b0e4c523768cbd2
ST-DDT <ST-DDT@gmx.de>
2014-02-08 12:10:15 +0100

    Store Material instead of materialid

--- src/main/java/de/st_ddt/crazyutil/geo/block/GeoBlock.java
+++ src/main/java/de/st_ddt/crazyutil/geo/block/GeoBlock.java
@@ -1,6 +1,7 @@
 package de.st_ddt.crazyutil.geo.block;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 
 import de.st_ddt.crazyutil.geo.location.GeoLocation;
@@ -8,14 +9,14 @@ import de.st_ddt.crazyutil.geo.location.GeoLocation;
 public class GeoBlock extends GeoLocation
 {
 
-	protected int material;
+	protected Material material;
 	protected byte data;
 
 	// protected BlockState state;
 	public GeoBlock(final Block block)
 	{
 		super(block.getX(), block.getY(), block.getZ());
-		material = block.getTypeId();
+		material = block.getType();
 		data = block.getData();
 		// state = block.getState();
 	}
@@ -23,6 +24,6 @@ public class GeoBlock extends GeoLocation
 	public void apply(final Location target, final boolean physics)
 	{
 		final Block block = target.clone().add(x, y, z).getBlock();
-		block.setTypeIdAndData(material, data, physics);
+		block.setTypeIdAndData(material.getId(), data, physics);
 	}
 }
